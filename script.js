196f46dffd37e3db57b849aa0de6ed37b371dc78
t0sh1t0sh1 <tosh1syog1@yahoo.co.jp>
2024-06-10 12:06:34 +0900

    入力フォームにjQueryを反映(途中)

--- script.js
+++ script.js
@@ -21,4 +21,101 @@ $(function(){
     autoplaySpeed: 5000,
     arrows: false,
   });
+
+  // 送信ボタンクリック時の処理
+  $('#submit').on('click', function(event){
+    // formtタグによる送信を拒否
+    event.preventDefault();
+
+    // 入力チェックした結果をresultに格納
+    let result = inputCheck();
+  });
+
+  // フォーカスが外れたとき（blur）にフォームの入力チェックをする
+  $('#name').blur(function () {
+    inputCheck();
+  });
+  $('#furigana').blur(function () {
+    inputCheck();
+  });
+  $('#email').blur(function () {
+    inputCheck();
+  });
+  $('#tel').blur(function () {
+    inputCheck();
+  });
+  $('#message').blur(function () {
+    inputCheck();
+  });
+  $('#agree').click(function () {
+    inputCheck();
+  });
+
+  // お問い合わせフォームの入力チェック
+  function inputCheck() {
+    // エラーのチェック結果
+    let result;
+
+    // エラーメッセージのテキスト
+    let message = '';
+
+    // エラーがなければfalse、エラーがあればtrue
+    let error = false;
+
+    // お名前のチェック
+    if($('#name').val() == '') {
+      // エラーあり
+      $('#name').css('background-color', '#f79999');
+      error = true
+      message += 'お名前を入力してください。\n';
+    } else {
+      // エラーなし
+      $('#name').css('background-color', '#fafafa');
+    }
+
+    // フリガナのチェック
+    if ($('#furigana').val() == '') {
+      // エラーあり
+      $('#furigana').css('background-color', '#f79999');
+      error = true;
+      message += 'フリガナを入力してください。\n';
+    } else {
+      // エラーなし
+      $('#furigana').css('background-color', '#fafafa');
+    }
+
+    // お問い合わせのチェック
+    if ($('#message').val() == '') {
+      // エラーあり
+      $('#message').css('background-color', '#f79999');
+      error = true;
+      message += 'お問い合わせ内容を入力してください。\n';
+    } else {
+      // エラーなし
+      $('#message').css('background-color', '#fafafa');
+    }
+
+    // メールアドレスのチェック
+    if ($('#email').val() == '' || $('#email').val().indexOf('@') == -1 || $('#email').val().indexOf('.') == -1) {
+      // エラーあり
+      $('#email').css('background-color', '#f79999');
+      error = true;
+      message += 'メールアドレスが未記入、または「@」「.」が含まれていません。\n';
+    } else {
+      // エラーなし
+      $('#email').css('background-color', '#fafafa');
+    }
+
+    // 電話番号のチェック（未入力はOK、未入力でない場合は-が必要）
+    if ($('#tel').val() != '' && $('#tel').val().indexOf('-') == -1) {
+      // エラーあり
+      $('#tel').css('background-color', '#f79999');
+      error = true;
+      message += '電話番号に「-」が含まれていません。\n';
+    } else {
+      // エラーなし
+      $('#tel').css('background-color', '#fafafa');
+    }
+  }
+
 });
